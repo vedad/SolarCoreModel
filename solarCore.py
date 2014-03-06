08dd91f0ddc609962d89a9df18dab57f6d4fa252
Vedad Hodzic <vedadh@student.matnat.uio.no>
2014-03-06 20:03:12 +0100

    Wrote integration loop. No errors yet.

--- solarCore.py
+++ solarCore.py
@@ -1,13 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def readOpacity(T, rho):
+def opacity(T, rho):
 	"""
-	Reads opacities from a file. Stores the
-	logarithm of temperature, radius and opacity
-	in separate arrays. Finds the opacity value
-	that most closely resembles the present values
-	for T and R.
+	Reads opacities from a file. Finds the opacity value
+	that most closely resembles the present values for
+	T and R.
+	Returns the opacity in units of [cm^2 g^-1].
 	"""
 
 	logT = []; logK = []
@@ -38,7 +37,7 @@ def readOpacity(T, rho):
 	i = np.argmin(diffT)
 	j = np.argmin(diffR)
 	
-	return 10**(logK[i,j])	# Returns the value of khappa with T and R closest to actual values.
+	return 10**(logK[i,j])
 
 
 def energyGeneration(T, rho):
@@ -46,12 +45,14 @@ def energyGeneration(T, rho):
 	Function to find the full energy generation per
 	unit mass from the three PP chain reactions.
 	"""
-	mu = 1 # Choose to use the atomic mass unit as unit mass
+#	mu = 1.6605e-27		# Units of [kg]		SI
+	mu = 1.6605e-24		# Units of [g]		CGS
+#	mu = 1				# Units of [u]
 
 	# Abundancy of different elements
-	X = 0.7				# Hydrogen (ionized)
-	Y = 0.29			# Helium 4 (ionized)
-	Y_3 = 1e-10			# Helium 3 (ionized)
+	X = 0.7				# Hydrogen (ionised)
+	Y = 0.29			# Helium 4 (ionised)
+	Y_3 = 1e-10			# Helium 3 (ionised)
 	Z = 0.01			# Heavier elements than the above
 	Z_7Li = 1e-5		# Lithium 7 (part of Z)
 	Z_7Be = 1e-5		# Beryllium 7 (part of Z)
@@ -68,7 +69,7 @@ def energyGeneration(T, rho):
 
 	### Number densities (n) ###
 	n_p = X * rho /	mu				# Hydrogen
-	n_3He = Y_3 * rho / (3 * mu)		# Helium 3
+	n_3He = Y_3 * rho / (3 * mu)	# Helium 3
 	n_4He = Y * rho / (4 * mu)		# Helium 4
 	n_7Be = Z_7Be * rho / (7 * mu)	# Beryllium 7
 	n_e = n_p + 2 * n_4He			# Electron
@@ -109,17 +110,57 @@ def energyGeneration(T, rho):
 
 	return epsilon
 
+def rho(T, P):
+	"""
+	Calculates the density at present location.
+	Returns density in units of [g cm^-3].
+	"""
+	### Abundancy ###
+	X = 0.7
+	Y = 0.29
+	Z = 0.01
+
+	### Constants ###
+
+	# Stefan-Boltzmann constant
+#	sigma = 5.67e-8		# Units of [W m^-2 K^-4]		SI
+	sigma = 5.67e-5	# Units of [erg cm^-2 s^-1 K^-4]	CGS
+
+	# Boltzmann constant
+#	k = 1.38e-23			# Units of [J K^-1]			SI
+	k = 1.38e-16			# Units of [erg K^-1]		CGS
+
+	# Atonmic mass unit
+#	m_u = 1.6605e-27		# Units of [kg]				SI
+	m_u = 1.6605e-24		# Units of [g]				CGS
+#	m_u = 1					# Units of [u]
+
+	# Speed of light
+#	c = 3e8					# Units of [m s^-1]			SI
+	c = 3e10				# Units of [cm s^-1]		CGS
+
+	# Radiation constant
+	a = 4 * sigma / c
+
+	# Average molecular weight
+	mu = 1./(2 * X + 3. / 4 * Y + Z / 2)
+	print mu
+
+	rho = mu * m_u / (k * T) * (P - a/3. * T*T*T*T)
+	return rho
+
 def drdm(r, rho):
 	"""
 	Calculates the right-hand side of dr/dm.
 	"""
 	return 1./(4 * np.pi * r * r * rho)
 
-def dPdm(r):
+def dPdm(r, m):
 	"""
 	Calculates the right-hand side of dP/dm.
 	"""
-	G = 6.67e-11	# Units of [N m kg^-2]
+#	G = 6.67e-11	# Units of [N m kg^-2]			SI
+	G = 6.67e-8		# Units of [cm^3 g^-1 s^-2]		CGS
 	return - G * m / (4 * np.pi * r * r * r * r)
 
 def dLdm(T, rho):
@@ -128,12 +169,68 @@ def dLdm(T, rho):
 	"""
 	return energyGeneration(T, rho)
 
-def dTdm(T):
+def dTdm(T, L, r, khappa):
 	"""
 	Calculates the right-hand side of dT/dm.
 	"""
-	sigma = 5.67e-8	# Units of [W m^-2 K^-4]
-	return - 3 * k * L / (256 * np.pi*np.pi * sigma * r*r*r*r * T*T*T)
+#	sigma = 5.67e-8			# Units of [W m^-2 K^-4]			SI
+	sigma = 5.67e-5			# Units of [erg cm^-2 s^-1 K^-4]	CGS
 
+	return -3 * k * L / (256 * np.pi*np.pi * sigma * r*r*r*r * T*T*T)
+
+
+def integration(T, rho, khappa, dm):
+	"""
+	Function that integrates the equation governing
+	the internal structure of the radiative core
+	of the Sun.
+	"""
+	
+#	L0 = 3.839e26			# Units of [J s^-1]			SI
+	L0 = 3.839e33			# Units of [erg s^-1]		CGS
+
+#	R0 = 0.5 * 6.96e8		# Units of [m]				SI
+	R0 = 0.5 * 6.96e10		# Units of [cm]				CGS
+
+#	M0 = 0.7 * 1.99e30		# Units of [kg]				SI
+	M0 = 0.7 * 1.99e33		# Units of [g]				CGS
+
+	T0 = 1e5				# Units of [K]				SI, CGS
+
+#	P0 = 1e11				# Units of [Pa]				SI
+	P0 = 1e12				# Units of [Ba]				CGS
+
+	rho0 = rho(T0, P0)		# Units of [g cm^-3]		CGS
+
+	n = int(round(M0 / dm))	# Length of integration arrays
+
+	# Initialising integration arrays
+	m = np.array(n)
+	m[0] = M0
+
+	r = np.array(n)
+	r[0] = R0
+
+	P = np.array(n)
+	P[0] = P0
+
+	L = np.array(n)
+	L[0] = L0
+
+	T = np.array(n)
+	T[0] = T0
+
+
+	for i in range(n):
+		if r[i] < 0 or L[i] < 0 or m[i] < 0:
+			break
+		else:
+			r[i+1] = r[i] + drdm(r[i], rho(T[i], P[i])) * dm
+			P[i+1] = P[i] + dPdm(r[i+1],m[i]) * dm
+			L[i+1] = L[i] + dLdm(T[i], rho(T[i], P[i])) * dm
+			T[i+1] = T[i] + dTdm(T[i], L[i+1], r[i+1], opacity(T[i], rho[i])) * dm
+			m[i+1] = m[i] - dm
+
+	pass
 
 
